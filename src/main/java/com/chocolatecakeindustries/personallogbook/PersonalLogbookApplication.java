9e5126be33e266643e79e4ededc8704b7b561892
Chris Gordon <cg51@st-andrews.ac.uk>
2022-07-14 14:22:42 +0100

    Setting up logbook project.

---
+++ src/main/java/com/chocolatecakeindustries/personallogbook/PersonalLogbookApplication.java
@@ -0,0 +1,13 @@
+package com.chocolatecakeindustries.personallogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLogbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocolatecakeindustries/personallogbook/PersonalLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocolatecakeindustries.personallogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLogbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
